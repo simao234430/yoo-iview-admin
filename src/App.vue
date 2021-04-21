68ff70b1dde8bdecc7a6f554c30b7f5c34c940b6
simao234430 <simao234430@gmail.com>
2021-04-21 19:09:26 +0800

    eslint配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -9,10 +12,10 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    'name': 'App',
+    'components': {
+        HelloWorld
+    }
 }
 </script>
 
